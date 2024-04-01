3f0983c22e82dfc1a4fd09b10ea9a232e5310d17
hong sipyo <155404215+SipYoHONG@users.noreply.github.com>
2024-04-01 17:07:09 +0900

    과제완

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -1,35 +1,40 @@
 import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-around', 
+    width: '60%',
+  },
+  countText: {
+    fontSize: 40,
+    marginBottom: 20,
+  },
+});
+
 export default function Counter() {
   const [count, setCount] = useState(0); 
-  
-  const handleClick = () => {
-    if(count < 5) {
-      setCount(count + 1);
-    }
-  };
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    countText: {
-      fontSize: 40,
-      marginBottom: 20,
-    },
-  });
 
   return (
     <View style={styles.container}>
-      <Text style={styles.countText}>{count}</Text>
+    <Text style={styles.countText}>{count < 5 ? '으쨔!' : '멈춰!!'}</Text>
+    <View style={styles.buttonContainer}>
       <Button 
-        onPress={handleClick} 
+        onPress={() => setCount(count + 1)} 
         title="증가하기" 
         disabled={count >= 5}
       />
+      <Button 
+        onPress={() => setCount(0)} 
+        title="리셋" 
+      />
     </View>
-  );
+  </View>
+);
 };
